25486bac0bc785cb3ce0052c0f7a79589612dee1
Lucasnag <Lucasnagomez@gmail.com>
2021-01-25 06:40:30 +0100

    el segundo commit

--- ticTac.js
+++ ticTac.js
@@ -1,12 +1,8 @@
 const statusDisplay = document.querySelector('.game--status');
 
-// var nameUser = prompt('Elije nombre jugador 1', "")
-// var nameUser2 = prompt('Elije nombre jugador 2', "")
-
-
-let gameActive = true;
-let currentPlayer = "X";
-let gameState = ["","","","","","","","","","","","","","","",];
+var gameActive = true;
+var currentPlayer = "X";
+var gameState = ["","","","","","","","",""];
 
 const winningMessage = () => `Player ${currentPlayer} has won!`;
 const drawMessage = () => `Game ended in a draw!`;
@@ -15,30 +11,14 @@ const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 statusDisplay.innerHTML = currentPlayerTurn();
 
 const winningConditions = [
-    [0, 1, 2, 3],
-    [1, 2, 3, 4],
-    [2, 3, 4, 5],
-    [6, 7, 8, 9],
-    [7, 8, 9, 10],
-    [8, 9, 10, 11],
-    [12, 13, 14, 15],
-    [13, 14, 15, 16],
-    [14, 15, 16, 17],
-    [18, 19, 20, 21],
-    [19, 20, 21, 22],
-    [20, 21, 22, 23],
-    [0, 7, 14, 21],
-    [1, 8, 15, 22],
-    [2, 9, 16, 23],
-    [3, 8, 13, 18],
-    [4, 9, 14, 19],
-    [5, 10, 15, 20],
-    [0, 6, 12, 18],
-    [1, 7, 13, 19],
-    [2, 8, 14, 20],
-    [3, 9, 15, 21],
-    [4, 10, 16, 22],
-    [5, 11, 17, 23],    
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]    
 ];
 
 function handleCellPlayed(clickedCell, clickedCellIndex) {
@@ -53,39 +33,17 @@ function handlePlayerChange() {
 
 function handleResultValidation() {
     let roundWon = false;
-    for (let i = 0; i <= 23; i++) {
+    for (let i = 0; i <= 7; i++) {
         const winCondition = winningConditions[i];
         let a = gameState[winCondition[0]];
         let b = gameState[winCondition[1]];
         let c = gameState[winCondition[2]];
-        let d = gameState[winCondition[3]];
-        let e = gameState[winCondition[4]];
-        let f = gameState[winCondition[5]];
-        let g = gameState[winCondition[6]];
-        let h = gameState[winCondition[7]];
-        let j = gameState[winCondition[8]];
-        let k = gameState[winCondition[9]];
-        let l = gameState[winCondition[10]];
-        let m = gameState[winCondition[11]];
-        let n = gameState[winCondition[12]];
-        let o = gameState[winCondition[13]];
-        let p = gameState[winCondition[14]];
-        let q = gameState[winCondition[15]];
-        let r = gameState[winCondition[16]];
-        let s = gameState[winCondition[17]];
-        let t = gameState[winCondition[18]];
-        let u = gameState[winCondition[19]];
-        let v = gameState[winCondition[20]];
-        let w = gameState[winCondition[21]];
-        let x = gameState[winCondition[22]];
-        let y = gameState[winCondition[23]];
-
-        if (a === '' || b === '' || c === ''|| d === '' || e === '' || f === ''|| g === '' || h === ''|| j === '' 
-        || k === '' || l === ''|| m === ''|| n === '' || o === ''|| p === '' || q === '' || r === ''|| s === ''|| 
-        t === '' || u === ''|| v === ''|| w === '' || x === ''|| y === '') {
+        
+        if (a === '' || b === '' || c === '') {
             continue;
         }
-        if (roundWon = true) {
+        if (a === b && b === c) {
+            roundWon = true;
             break;
         }
     }
